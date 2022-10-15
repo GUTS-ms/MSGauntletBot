111c4409a1f05756f1683ba64de5f939089010e0
Lemish891 <2469780m@student.gla.ac.uk>
2022-10-15 13:43:04 +0100

    a star algorithm

--- Bot/visBot.py
+++ Bot/visBot.py
@@ -3,6 +3,7 @@ import socket
 import time
 import random
 import numpy as np
+import heapq
 
 from mpltlibstyle import *
 
@@ -90,6 +91,77 @@ def SendMessage(requestmovemessage):
     bytesToSend = str.encode(requestmovemessage)
     UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
+def heuristic(a, b):
+        return np.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
+
+def make_step(posx,posy):
+    start = (posy,posx)
+    goal = (50,50)
+    route = astar(botmap, start, goal)
+    route = route + [start]
+    route = route[::-1]
+    print(route)
+    x_coords = []
+    y_coords = []
+
+    for i in (range(0,len(route))):
+        x = route[i][0]
+        y = route[i][1]
+        x_coords.append(x)
+        y_coords.append(y)
+
+        # plot map and path
+    fig, ax = plt.subplots(figsize=(20,20))
+    ax.imshow(botmap, cmap=plt.cm.Dark2)
+    ax.scatter(start[1],start[0], marker = "*", color = "yellow", s = 200)
+    ax.scatter(goal[1],goal[0], marker = "*", color = "red", s = 200)
+    ax.plot(y_coords,x_coords, color = "black")
+    plt.show()
+
+def astar(array, start, goal):
+    neighbors = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
+    close_set = set()
+    came_from = {}
+    gscore = {start:0}
+    fscore = {start:heuristic(start, goal)}
+    oheap = []
+    heapq.heappush(oheap, (fscore[start], start))
+
+    while oheap:
+        current = heapq.heappop(oheap)[1]
+        if current == goal:
+            data = []
+            while current in came_from:
+                data.append(current)
+                current = came_from[current]
+            return data
+        close_set.add(current)
+
+        for i, j in neighbors:
+            neighbor = current[0] + i, current[1] + j
+            tentative_g_score = gscore[current] + heuristic(current, neighbor)
+            if 0 <= neighbor[0] < array.shape[0]:
+                if 0 <= neighbor[1] < array.shape[1]:                
+                    if array[neighbor[0]][neighbor[1]] == 1:
+                        continue
+                else:
+                    # array bound y walls
+                    continue
+            else:
+                # array bound x walls
+                continue
+    
+            if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
+                continue
+
+            if  tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in oheap]:
+                came_from[neighbor] = current
+                gscore[neighbor] = tentative_g_score
+                fscore[neighbor] = tentative_g_score + heuristic(neighbor, goal)
+                heapq.heappush(oheap, (fscore[neighbor], neighbor))
+
+    return []
+
 startcount = 0
 
 while True:
@@ -132,10 +204,9 @@ while True:
             wallcoords.append((wallsSplit[i],wallsSplit[i+1]))
         for coords in wallcoords:
             if coords not in seen_walls:
-                if coords in find_player_neighbors(posxby8,posyby8):
-                    seen_walls.append(coords)
-                    botmap[int(int(coords[1])/8),int(int(coords[0])/8)]=1
-        show(botmap)
+                seen_walls.append(coords)
+                botmap[int(int(coords[1])/8),int(int(coords[0])/8)]=1
+        # show(botmap)
         
     if "nearbyfloors" in msgFromServer:
         print(msgFromServer)
@@ -152,28 +223,8 @@ while True:
         show(botmap)
     now = time.time()
 
-    def make_step():
-        #print(int(int(posy)/8),int(int(posx)/8))
-        if botmap[(int(int(posy)/8))-1][int(int(posx)/8)] != 1:
-            requestmovemessage = "moveto:" + str(posx - 8)  + "," + str(posy)
-            SendMessage(requestmovemessage)
-        elif botmap[int(int(posy)/8)][(int(int(posx)/8))-1] != 1:
-            requestmovemessage = "moveto:" + str(posx)  + "," + str(posy-8)
-            SendMessage(requestmovemessage)
-        elif botmap[(int(int(posy)/8))+1][int(int(posx)/8)] != 1:
-            requestmovemessage = "moveto:" + str(posx + 8)  + "," + str(posy)
-            SendMessage(requestmovemessage)
-        elif botmap[int(int(posy)/8)][(int(int(posx)/8))+1] != 1:
-            requestmovemessage = "moveto:" + str(posx)  + "," + str(posy + 8)
-            SendMessage(requestmovemessage)
-        else:
-            randomDirection = random.choice(directions)
-            directionMoveMessage = "movedirection:" + randomDirection
-            SendMessage(directionMoveMessage)
-    now = time.time()
-
     #every few seconds, request to move to a random point nearby. No pathfinding, server will 
     #attempt to move in straight line.
     if (now - timeSinceMove) > moveInterval:
         find_unexp(botmap)
-        make_step()
+        make_step(posyby8,posxby8)
